4d122113d59e4d64c12368f974925bb3327b3afb
Tobias Brandner <tobias.brandner@stud-mail.uni-wuerzburg.de>
2023-08-23 11:35:48 +0200

    init Custom Coop

---
+++ general/arguments.py
@@ -0,0 +1,43 @@
+class Arguments:
+    root: str = ""
+    output_dir: str = ""
+    resume: str = ""
+    seed: int = -1
+    source_domains: str = None
+    target_domains: str = None
+    transforms: str = None
+    config_file: str = "configs/trainers/CoOp/"
+    dataset_config_file: str = "configs/datasets/"
+    trainer: str = "CoOp"
+    backbone: str = ""
+    head: str = ""
+    eval_only: bool = False
+    model_dir: str = ""
+    load_epoch: int = None
+    no_train: bool = False
+    n_ctx: int = 16
+    csc: bool = False
+    ctp: str = "end"
+    shots: int = 1
+    opts: list = [] # opts=['TRAINER.COOP.N_CTX', '16', 'TRAINER.COOP.CSC', 'False', 'TRAINER.COOP.CLASS_TOKEN_POSITION', 'end', 'DATASET.NUM_SHOTS', '1']
+
+    def __init__(self, trainer: str, root: str, dataset: str, config: str, ctp: str, n_ctx: int, shots: int, csc: bool, output_dir: str):
+        self.trainer = str(trainer)
+        self.root = str(root)
+        self.dataset_config_file += str(dataset) + ".yaml"
+        self.config_file += str(config) + ".yaml"
+        self.ctp = str(ctp)
+        self.n_ctx = n_ctx
+        self.shots = shots
+        self.csc = csc
+        self.opts = ['TRAINER.COOP.N_CTX', str(n_ctx), 'TRAINER.COOP.CSC', str(csc), 'TRAINER.COOP.CLASS_TOKEN_POSITION',
+                     str(ctp), 'DATASET.NUM_SHOTS', str(shots)]
+        self.output_dir = output_dir
+        # self.seed = seed
+
+    def __str__(self):
+        return (f"backbone={self.backbone}, config_file={self.config_file}, dataset_config_file={self.dataset_config_file}, "
+                f"eval_only={self.eval_only}, head={self.head}, load_epoch={self.load_epoch}, model_dir={self.model_dir}, "
+                f"no_train={self.no_train},  opts: {self.opts}, output_dir={self.output_dir}, resume={self.resume}, "
+                f"root={self.root}, seed={self.seed}, source_domains={self.source_domains}, "
+                f"target_domains={self.target_domains}, trainer={self.trainer}, transforms={self.transforms}")
---
+++ train.py
@@ -0,0 +1,167 @@
+import torch
+
+from dassl.utils import setup_logger, set_random_seed, collect_env_info
+from dassl.config import get_cfg_default
+from dassl.engine import build_trainer
+
+# Test
+from general.arguments import Arguments
+
+# custom
+import datasets.oxford_pets
+import datasets.oxford_flowers
+import datasets.fgvc_aircraft
+import datasets.dtd
+import datasets.eurosat
+import datasets.stanford_cars
+import datasets.food101
+import datasets.sun397
+import datasets.caltech101
+import datasets.ucf101
+import datasets.imagenet
+
+import datasets.imagenet_sketch
+import datasets.imagenetv2
+import datasets.imagenet_a
+import datasets.imagenet_r
+
+import trainers.coop
+import trainers.cocoop
+import trainers.zsclip
+
+
+def print_args(args, cfg):
+    print("***************")
+    print("** Arguments **")
+    print("***************")
+    optkeys = list(args.__dict__.keys())
+    optkeys.sort()
+    for key in optkeys:
+        print("{}: {}".format(key, args.__dict__[key]))
+    print("************")
+    print("** Config **")
+    print("************")
+    print(cfg)
+
+
+def reset_cfg(cfg, args):
+    if args.root:
+        cfg.DATASET.ROOT = args.root
+
+    if args.output_dir:
+        cfg.OUTPUT_DIR = args.output_dir
+
+    if args.resume:
+        cfg.RESUME = args.resume
+
+    if args.seed:
+        cfg.SEED = args.seed
+
+    if args.source_domains:
+        cfg.DATASET.SOURCE_DOMAINS = args.source_domains
+
+    if args.target_domains:
+        cfg.DATASET.TARGET_DOMAINS = args.target_domains
+
+    if args.transforms:
+        cfg.INPUT.TRANSFORMS = args.transforms
+
+    if args.trainer:
+        cfg.TRAINER.NAME = args.trainer
+
+    if args.backbone:
+        cfg.MODEL.BACKBONE.NAME = args.backbone
+
+    if args.head:
+        cfg.MODEL.HEAD.NAME = args.head
+
+
+def extend_cfg(cfg):
+    """
+    Add new config variables.
+
+    E.g.
+        from yacs.config import CfgNode as CN
+        cfg.TRAINER.MY_MODEL = CN()
+        cfg.TRAINER.MY_MODEL.PARAM_A = 1.
+        cfg.TRAINER.MY_MODEL.PARAM_B = 0.5
+        cfg.TRAINER.MY_MODEL.PARAM_C = False
+    """
+    from yacs.config import CfgNode as CN
+
+    cfg.TRAINER.COOP = CN()
+    cfg.TRAINER.COOP.N_CTX = 16  # number of context vectors
+    cfg.TRAINER.COOP.CSC = False  # class-specific context
+    cfg.TRAINER.COOP.CTX_INIT = ""  # initialization words
+    cfg.TRAINER.COOP.PREC = "fp16"  # fp16, fp32, amp
+    cfg.TRAINER.COOP.CLASS_TOKEN_POSITION = "end"  # 'middle' or 'end' or 'front'
+
+    cfg.TRAINER.COCOOP = CN()
+    cfg.TRAINER.COCOOP.N_CTX = 16  # number of context vectors
+    cfg.TRAINER.COCOOP.CTX_INIT = ""  # initialization words
+    cfg.TRAINER.COCOOP.PREC = "fp16"  # fp16, fp32, amp
+
+    # cfg.TRAINER.ZeroshotCLIP.PREC = "fp32"
+
+    cfg.DATASET.SUBSAMPLE_CLASSES = "all"  # all, base or new
+
+
+def setup_cfg(args):
+    cfg = get_cfg_default()
+    extend_cfg(cfg)
+
+    # 1. From the dataset config file
+    if args.dataset_config_file:
+        cfg.merge_from_file(args.dataset_config_file)
+
+    # 2. From the method config file
+    if args.config_file:
+        cfg.merge_from_file(args.config_file)
+
+    # 3. From input arguments
+    reset_cfg(cfg, args)
+
+    # 4. From optional input arguments
+    cfg.merge_from_list(args.opts)
+
+    cfg.freeze()
+
+    return cfg
+
+
+def main(args):
+    cfg = setup_cfg(args)
+    if cfg.SEED >= 0:
+        print("Setting fixed seed: {}".format(cfg.SEED))
+        set_random_seed(cfg.SEED)
+    # setup_logger(cfg.OUTPUT_DIR)
+
+    if torch.cuda.is_available() and cfg.USE_CUDA:
+        torch.backends.cudnn.benchmark = True
+
+    print_args(args, cfg)
+    print("Collecting env info ...")
+    print("** System info **\n{}\n".format(collect_env_info()))
+
+    trainer = build_trainer(cfg)
+
+    if args.eval_only:
+        trainer.load_model(args.model_dir, epoch=args.load_epoch)
+        trainer.test()
+        return
+
+    if not args.no_train:
+        trainer.train()
+
+
+if __name__ == "__main__":
+    args = Arguments("CoOp", "/home/brandnerkasper/Uni/MP/CustomCoOp/data", "caltech101",
+                      "rn50_ep50", "end", 16, 1, False, "output/Caltech")
+
+    setup_logger(args.output_dir)
+
+    for i in 1, 2, 3:
+        args.seed = i
+        args.output_dir = args.output_dir + "/" + str(i)
+        print(f"args2: {args}")
+        main(args)
